315934c9550c4841bbbf4c8fc1effc3d3a6998c1
harnil <harnilkadia99@gmail.com>
2023-02-09 10:19:41 +0530

    finally

---
+++ p1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $a=12;
+        $b=3;
+        echo"Multiplication of $a and $b is ".$a*$b."<br>";
+        echo"Subtraction of $a and $b is ".$a-$b."<br>";
+        echo"Division of $a and $b is ".$a/$b."<br>";
+        echo"Addition of $a and $b is ".$a+$b."<br>";
+    ?>
+</body>
+</html>
